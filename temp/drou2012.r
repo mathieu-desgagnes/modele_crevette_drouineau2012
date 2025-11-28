c49c364c6438b649cec69b4799e2616a63cde2d5
Desgagnés <Mathieu.Desgagnes@dfo-mpo.gc.ca>
2025-11-28 10:03:05 -0500

    Creation du package

--- temp/drou2012.r
+++ temp/drou2012.r
@@ -6,55 +6,12 @@
 ## ####
 
 library('RTMB')
-library('here')
+# library('here')
 
 anneesFittees <- 1990:2007
 origineDonnees <- ifelse(TRUE, 'drou2012', '') #TRUE si données de drouineau2012, FALSE sinon
 
 ## charger les données
-calculerData <- function(annees=1990:2007, new_strata=0){
-    ## lecture des fichiers d'observation
-    ##
-    data <- list()
-    data$anneesFittees <- annees; names(data$anneesFittees) <- data$anneesFittees
-    data$nbAge <- 4 #age max de mâles, changement de sexe obligatoire ensuite
-    data$saisons <- as.data.frame(list(nom=c('a','b','c','d'), nbMois=c(2,3,4,3))); dimnames(data$saisons)[[1]] <- data$saisons$nom
-    data$midTaille <- seq(0, 29.5, by=0.5)+0.25; names(data$midTaille) <- data$midTaille #classe de tailles de 0.5cm
-    ##
-    ##
-    ## 4 saison ("pas de temp") débutant au printemps
-    data$nbPasDeTemps <- length(data$anneesFittees)*4
-    temp <- expand.grid(annee=anneesFittees,saison=data$saisons$nom)
-    temp <- temp[order(temp$annee, temp$saison),]
-    temp$nom <- paste0(temp$annee, temp$saison)
-    data$nomPasDeTemps <- temp
-    ## effort commercial
-    effort.init <- read.csv2(file=here('data', origineDonnees, 'effort_comm.csv'))
-    effort.init$val <- effort.init$effort; effort.init$effort <- NULL
-    effort.init$saison <- c('a','b','c','d')[effort.init$saison]
-    data$effort_comm <- effort.init
-    ##
-    fl_abond.init <- read.csv2(file=here('data', origineDonnees, 'dfl_rel.csv'))
-    fl_abond.male <- fl_abond.init[,1:2]; fl_abond.male$mids <- as.numeric(substr(dimnames(fl_abond.init)[[2]][3],2,10)); fl_abond.male$val <- fl_abond.init[,3]
-    for(i.cl in 4:(ncol(fl_abond.init)-3)){
-        temp <- fl_abond.init[,1:2]
-        temp$mids <- as.numeric(substr(dimnames(fl_abond.init)[[2]][i.cl],2,10))
-        temp$val <- fl_abond.init[,i.cl]
-        fl_abond.male <- rbind(fl_abond.male, temp)
-    }
-    data$fl_rel.male <- fl_abond.male
-    ##
-    fl_abond.femelle <- fl_abond.init[,1:2]; fl_abond.femelle$stade <- 'femelle'; fl_abond.femelle$val <- fl_abond.init[,'femelles']
-    for(i.cl in c('primi','multi')){
-        temp <- fl_abond.init[,1:2]
-        temp$stade <- i.cl
-        temp$val <- fl_abond.init[,i.cl]
-        fl_abond.femelle <- rbind(fl_abond.femelle, temp)
-    }
-    data$fl_rel.femelle <- fl_abond.femelle
-    ##
-    data
-}
 data <- calculerData(annees=1990:2007)
 
 calculerParam <- function(data){
